c6733d7b1e45a376f5709123fe6447be6a64e229
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2022-06-29 01:16:52 +0600

    Add files via upload

---
+++ Adding New Column in a Timeseries/addColumn.R
@@ -0,0 +1,17 @@
+library(zoo) 
+library(xts) 
+Sys.setenv(TZ="Asia/Dhaka") 
+
+#Hourly time series 
+timeseries <- seq(from=as.POSIXct("2010-01-13 00:00:00 BST"), to=as.POSIXct("2010-03-13 23:59:59 BST"), by="hour") 
+value <- rnorm(n=length(timeseries)) 
+irreg <- xts(value, order.by = timeseries)
+head(irreg)
+
+data_matrix = as.matrix(coredata(irreg))
+as.data.frame(data_matrix)
+
+value <- rnorm(n=row(df))
+df$newColumn = value
+
+irreg <- xts(x = df, order.by = timeseries)
