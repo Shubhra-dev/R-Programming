2f09fcd20f86d27173860d7b244304402837e79e
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2022-07-01 13:14:14 +0600

    Add files via upload

---
+++ Converting Periodicity & Rolling Functions/Converting Periodicity_Rolling Functions.R
@@ -0,0 +1,26 @@
+library(xts) 
+library(zoo)
+
+df = read.csv("F:/df.csv",header = TRUE, stringsAsFactors = FALSE) 
+date <- seq(as.Date("2021-03-01"), length = 396, by = "days") 
+data = df$Value 
+# transform the data frame into time series object
+df = xts(x=data,order.by=date)
+
+df_week = to.period(df,period = "weeks")  
+head(df_week)
+
+#without OHLC
+df_week_2 = to.period(df,period = "weeks",OHLC = FALSE) 
+head(df_week_2)
+
+
+#Applying Rolling functions
+
+#a monthly cumulative sum of a time series
+df_monthly = split(df,f="months") 
+df_cumsum = lapply(df_monthly,FUN = cumsum) > head(df_cumsum)
+
+#standard deviation of the same time series data after every 3 units of period
+df_sd <- rollapply(df, width=3, FUN = sd) # width = the number of periods after the sd is calculated. 
+head(df_sd)
