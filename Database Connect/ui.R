37ced9245425af3db82a758adb102bc2e620e81d
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-03-01 23:17:28 +0600

    Link Removed

--- Database Connect/ui.R
+++ Database Connect/ui.R
@@ -4,7 +4,6 @@ shinyUI(fluidPage(
   titlePanel("File Input"),
   sidebarLayout(
     sidebarPanel(
-      tags$a(href="//shortstaysolution.com/intro/", "Instruction"),
       fileInput("file","Upload the file (CSV or Excel)"),
       textInput("link", "Upload link")
     ),
