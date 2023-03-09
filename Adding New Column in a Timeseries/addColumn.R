7fa58deafeeb5d31f88d9e1707894fa4bb42cd26
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-03-09 15:54:51 +0600

    Update addColumn.R

--- Adding New Column in a Timeseries/addColumn.R
+++ Adding New Column in a Timeseries/addColumn.R
@@ -2,15 +2,15 @@ library(zoo)
 library(xts) 
 Sys.setenv(TZ="Asia/Dhaka") 
 
-#Hourly time series 
+# Hourly time series 
 timeseries <- seq(from=as.POSIXct("2010-01-13 00:00:00 BST"), to=as.POSIXct("2010-03-13 23:59:59 BST"), by="hour") 
 value <- rnorm(n=length(timeseries)) 
 irreg <- xts(value, order.by = timeseries)
 head(irreg)
 
-#Converting as Matrix
+# Converting as Matrix
 data_matrix = as.matrix(coredata(irreg))
-#Again converting as Dataframe
+# Again converting as Dataframe
 as.data.frame(data_matrix)
 
 value <- rnorm(n=row(df))
