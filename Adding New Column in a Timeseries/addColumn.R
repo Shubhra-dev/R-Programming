d55a16080cc9051fc4fdc1653e2febe2c0174066
Shubhradev Chakrabarty <89873720+Shubhra-dev@users.noreply.github.com>
2023-03-12 22:59:25 +0600

    Update addColumn.R

--- Adding New Column in a Timeseries/addColumn.R
+++ Adding New Column in a Timeseries/addColumn.R
@@ -2,7 +2,7 @@ library(zoo)
 library(xts) 
 Sys.setenv(TZ="Asia/Dhaka") 
 
-# Hourly time series 
+# Hourly Time Series
 timeseries <- seq(from=as.POSIXct("2010-01-13 00:00:00 BST"), to=as.POSIXct("2010-03-13 23:59:59 BST"), by="hour") 
 value <- rnorm(n=length(timeseries)) 
 irreg <- xts(value, order.by = timeseries)
